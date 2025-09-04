35b14bc8dd1aed893c72160e39ab779b1a7ce7f1
naresh56007-lang <naresh56007@gmail.com>
2025-09-04 20:38:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/ Add fade-in effect on scroll
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-up');
+
+const appearOptions = {
+  threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add("appear");
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
